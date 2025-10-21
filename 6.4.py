a590d7b4ecc627bddc8840fc925a2527861c6b4d
Ivan-Sharapov <zynchy.88@gmail.com>
2025-10-21 19:08:31 +0300

    Add files via upload

---
+++ 6.4.py
@@ -0,0 +1,11 @@
+import math
+a = float(input())
+b = float(input())
+c = float(input())
+D = b**2 - 4*a*c
+if D < 0:
+    print("Нет корней")
+elif D == 0:
+    print(-b / (2*a))
+else:
+    print((-b + math.sqrt(D)) / (2*a), (-b - math.sqrt(D)) / (2*a))
